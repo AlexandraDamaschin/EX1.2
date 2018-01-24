298a7ece97743b6c62359cb1f930c53a9c14418a
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-24 11:25:22 +0000

    1. Create a new Order class in the Models Namespace

---
+++ Ex1.2/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Ex1._2.Models
+{
+    [Table("Order")]
+    public class Order
+    {
+        public DateTime DeliveredOn { get; set; }
+        public int ID { get; set; }
+        public DateTime OrderOn { get; set; }
+        public double Value { get; set; }
+    }
+}
